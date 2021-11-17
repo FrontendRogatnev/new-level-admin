3d1c914d892b433db8bfdfee5eb2ca7205fdb9f8
Frontend Rogatnev <frontend.rogatnev@gmail.com>
2021-11-17 21:43:45 +0300

    Init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd5ca75ac9a7c3019de079359098d8af0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6170d130-ff46-4400-9ac8-2e1da45e80f0'
+};
